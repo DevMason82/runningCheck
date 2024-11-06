7dbcdc05b581df5ff5455000bb689e9226fcab33
Mason <choimusong0529@gmail.com>
2024-11-06 11:18:51 +0900

    -카드별 상세내용 ui -쿠팡파트너스 연동

--- app/runningStatusInfo/components/modalDetail.tsx
+++ app/runningStatusInfo/components/modalDetail.tsx
@@ -8,6 +8,9 @@ import {
   ModalFooter,
   Button,
   useDisclosure,
+  Link,
+  Divider,
+  Chip,
 } from "@nextui-org/react";
 
 const ModalDetail = ({ isOpen, onClose, result, weatherIndex }) => {
@@ -18,7 +21,8 @@ const ModalDetail = ({ isOpen, onClose, result, weatherIndex }) => {
   console.log("selectedResult", selectedResult);
   return (
     <Modal
-      size="full"
+      backdrop="blur"
+      size="sm"
       isOpen={isOpen}
       onClose={onClose}
       className="text-default-700"
@@ -30,17 +34,82 @@ const ModalDetail = ({ isOpen, onClose, result, weatherIndex }) => {
               {selectedResult.condition}
             </ModalHeader>
             <ModalBody>
-              <p>{selectedResult.rating}</p>
-              <p>{selectedResult.recommendation.message}</p>
-              <p>{selectedResult.recommendation.items}</p>
+              <Chip
+                className={
+                  selectedResult.rating === "good"
+                    ? "bg-success-400"
+                    : selectedResult.rating === "caution"
+                    ? "bg-amber-300 text-black"
+                    : selectedResult.rating === "warning"
+                    ? "bg-warning-400"
+                    : "bg-danger-400"
+                }
+              >
+                {selectedResult.rating}
+              </Chip>
+              {/*<p>{selectedResult.rating}</p>*/}
+              <div>
+                <span className="font-semibold">메세지</span>
+                <p className="text-base ml-2">
+                  {selectedResult.recommendation.message}
+                </p>
+              </div>
+
+              {selectedResult.recommendation.items !== "" && (
+                <div>
+                  <span className="font-semibold">아이템</span>
+                  <p className="text-base ml-2">
+                    {selectedResult.recommendation.items}
+                  </p>
+                </div>
+              )}
+
+              {selectedResult.recommendation.shoes !== "" && (
+                <div>
+                  <span className="font-semibold">러닝슈즈</span>
+                  <p className="text-base ml-2">
+                    {selectedResult.recommendation.shoes}
+                  </p>
+                </div>
+              )}
+
+              <Divider />
+
+              {/*<iframe*/}
+              {/*  src="https://coupa.ng/cguRSF"*/}
+              {/*  width="120"*/}
+              {/*  height="240"*/}
+              {/*  frameBorder="0"*/}
+              {/*  scrolling="no"*/}
+              {/*  referrerPolicy="unsafe-url"*/}
+              {/*  browsingtopics*/}
+              {/*></iframe>*/}
             </ModalBody>
             <ModalFooter>
-              <Button color="danger" variant="light" onPress={onClose}>
-                Close
-              </Button>
-              <Button color="primary" onPress={onClose}>
-                Action
-              </Button>
+              <div>
+                <Button
+                  href="https://link.coupang.com/a/bZga9r"
+                  as={Link}
+                  color="success"
+                  showAnchorIcon
+                  size="sm"
+                  variant="solid"
+                  className="mb-2"
+                  isExternal
+                >
+                  쿠팡 홈
+                </Button>
+                <p className="text-xs">
+                  &quot;쿠팡 파트너스 활동의 일환으로, 이에 따른 일정액의
+                  수수료를 제공받습니다.&quot;
+                </p>
+              </div>
+              {/*<Button color="danger" variant="light" onPress={onClose}>*/}
+              {/*  Close*/}
+              {/*</Button>*/}
+              {/*<Button color="primary" onPress={onClose}>*/}
+              {/*  Action*/}
+              {/*</Button>*/}
             </ModalFooter>
           </>
         )}
