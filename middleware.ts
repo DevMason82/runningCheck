2bc289166352235018b8c572a6e761772ea2a440
Mason <choimusong0529@gmail.com>
2024-11-12 09:09:58 +0900

    미들웨어 수정

--- middleware.ts
+++ middleware.ts
@@ -4,30 +4,19 @@ import type { NextRequest } from "next/server";
 export { default, withAuth } from "next-auth/middleware";
 
 export function middleware(request: NextRequest) {
-  const token =
-    request.cookies.get("next-auth.session-token") ||
-    request.cookies.get("__Secure-next-auth.session-token"); // 토큰 가져오기
-  // const myPosition = request.cookies.get("myPosition"); // 쿠키 값 가져오기
+  const token = request.cookies.get("next-auth.session-token"); // 토큰 가져오기
   const currentPath = request.nextUrl.pathname; // 현재 요청 경로 가져오기
-  const searchParams = request.nextUrl.searchParams;
-  const targetCity = searchParams.get("city"); // ?city= 값 확인
-  // console.log("TARGETCITY", targetCity);
 
-  if (!token) {
+  console.log("Request session token", token);
+
+  if (token === undefined) {
+    console.log("sdfsdfsdfsd");
+    // 토큰이 없으면 `/signing` 경로로 리다이렉트
     return NextResponse.redirect(new URL("/signin", request.url));
   }
 
-  if (token && !targetCity && currentPath === "/signin") {
-    return NextResponse.redirect(new URL("/", request.url));
-  }
-
-  // 쿠키가 있고 현재 경로가 /runningStatusInfo가 아닌 경우에만 리다이렉트
-  if (targetCity && currentPath !== "/runningStatusInfo") {
-    return NextResponse.redirect(new URL("/runningStatusInfo", request.url));
-  }
-
-  // 쿠키가 없고 현재 경로가 /가 아닌 경우에만 리다이렉트
-  if (!targetCity && currentPath !== "/") {
+  if (currentPath === "/signin") {
+    // 토큰이 있지만 현재 경로가 `/signin`일 경우, 홈으로 리다이렉트
     return NextResponse.redirect(new URL("/", request.url));
   }
 
@@ -36,5 +25,5 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/runningStatusInfo/:path*", "/"], // 미들웨어 적용 경로
+  matcher: ["/", "/runningStatusInfo/:path*"], // 미들웨어 적용 경로
 };
