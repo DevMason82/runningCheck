1ecacbe0c2deafd600ae2a896f8a584f1db7820e
Mason <choimusong0529@gmail.com>
2024-11-14 17:39:51 +0900

    쿠팡 파트너스 banner add

--- app/runningStatusInfo/page.tsx
+++ app/runningStatusInfo/page.tsx
@@ -1,7 +1,8 @@
-import { Skeleton } from "@nextui-org/react";
+import { Divider, Skeleton } from "@nextui-org/react";
 
 import dynamic from "next/dynamic";
-import { getWeather, getWeather2 } from "@/app/actions";
+import { fetchCoupangRecommendations, getWeather2 } from "@/app/actions";
+import ProductCard from "@/components/productCard";
 
 const LazyWeather = dynamic(() => import("@/components/weather"), {
   loading: () => <Skeleton className="rounded-lg" />,
@@ -17,12 +18,13 @@ export default async function Page({
   // console.log("CITY NAME ==>>", cityName);
   // const data = await getWeather(city);
   const data = await getWeather2(latitude, longitude);
-  console.log("Weather DATA ==>>", data);
-  // const kmaData = await getUltraSrtNcst(nx, ny);
-  // console.log("kmaData ==>>", kmaData);
+  // console.log("Weather DATA ==>>", data);
+  const coupagngRecoData = await fetchCoupangRecommendations();
   return (
     <div className="max-w">
       <LazyWeather data={data} />
+      <Divider className="my-5" />
+      <ProductCard data={coupagngRecoData} recoNum={1} />
     </div>
   );
 }
